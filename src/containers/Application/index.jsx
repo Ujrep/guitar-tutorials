bb9a76ed5d6d5227b20eb231b0d3c221a8b1d460
Stefan Perju <stefan.perju@yahoo.com>
2020-11-03 15:30:47 +0200

    Update page

--- src/containers/Application/index.jsx
+++ src/containers/Application/index.jsx
@@ -5,8 +5,12 @@ import chords from './chords.png';
 const Application = () => {
   return (
     <div>
-      Chords
-      <img src={chords} alt="chords" />
+      <span>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/izSLT-ZaiKA" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullscreen />
+      </span>
+      <span>
+        <img src={chords} alt="chords" />
+      </span>
     </div>
   );
 };
